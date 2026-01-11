540a32300903dc0484f10d80b8fba4f4ef08e48e
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-01-11 23:47:03 +0100

    Update javascriptBasics.js

--- javascriptBasics.js
+++ javascriptBasics.js
@@ -228,3 +228,43 @@ describe("Tests", () => {
     assert.strictEqual(saleHotdogs(100), 9000);
   });
 });
+
+
+/*
+  Training JS #8: Conditional statement--switch
+*/
+
+function howManydays(month){
+  var days;
+  switch (month){
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        days = 31;
+        break;
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+        days = 30;
+        break;
+      case 2:
+        days = 28;
+        break;
+  }
+  return days;
+}
+
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertSimilar(howManydays(1),31);
+    Test.assertSimilar(howManydays(2),28);
+    Test.assertSimilar(howManydays(3),31);
+    Test.assertSimilar(howManydays(4),30);
+    Test.assertSimilar(howManydays(12),31);
+  });
+});
